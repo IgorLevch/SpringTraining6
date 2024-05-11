78627e2595d57c275b8a15927c8d49b0a0145462
IgorLevch <igorlevvv@gmail.com>
2024-05-11 13:14:18 +0300

    8 урок Алишева
    Методы:
    - init();
    - destroy();
    - factory();

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,9 +12,26 @@ public class Main {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext
                 ("applicationContext.xml");
 
+       ClassicalMusic music = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(music.play());
+
+          // далее ДЗ
+        music.setVolume(150);
+        ClassicalMusic music1 = context.getBean("musicBean",ClassicalMusic.class);
+        ClassicalMusic music2 =context.getBean("musicBean",ClassicalMusic.class);
+        System.out.println(music.getVolume());
+
+        System.out.println(music1.play());
+        System.out.println(music2.play());
+
+        System.out.println(music1.getVolume());
+        System.out.println(music2.getVolume());
 
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+
+
+      /*  MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         boolean comparison = firstMusicPlayer == secondMusicPlayer;
@@ -26,7 +43,7 @@ public class Main {
         firstMusicPlayer.setVolume(10);
 
         System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());*/
 
    /*     musicPlayer.playMusic();
         System.out.println(musicPlayer.getName());
