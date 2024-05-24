fb688b2538e747285fe107da47e56e53c6455822
IgorLevch <igorlevvv@gmail.com>
2024-05-24 17:34:01 +0300

    11 урок Алишева
    Аннотации.
    Аннотация @Qualifired
    Внедрение зависимостей через аннотации.
    Без ДЗ.

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,29 +1,28 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 @Component
 public class MusicPlayer {
+   /* @Autowired
+    @Qualifier("rockMusic")*/
+    private Music music1;
+  /*  @Autowired
+    @Qualifier("popMusic")*/
+    private Music music2;
 
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer( @Qualifier("popMusic") Music music1,  @Qualifier("rockMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
-
-    /*@Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic) {
-        this.classicalMusic = classicalMusic;
-    }*/
-
     public String playMusic(){
-        return "Playing: " + classicalMusic.play();
+        return "Playing: " + music1.play()+", "+music2.play();
 
     }}
 
